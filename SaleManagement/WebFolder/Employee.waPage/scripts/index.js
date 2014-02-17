cc123660ad4a4c81031bfa2fdd2df576fc84ceae
minh <minhminh265@gmail.com>
2014-02-17 17:39:09 +0700

    new add very cuoi

---
+++ SaleManagement/WebFolder/Employee.waPage/scripts/index.js
@@ -0,0 +1,19 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var btnAddNew = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	btnAddNew.click = function btnAddNew_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		$$('empcontainer').selectTab(2);
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("btnAddNew", "click", btnAddNew.click, "WAF");
+// @endregion
+};// @endlock
