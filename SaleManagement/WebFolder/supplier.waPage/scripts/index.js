19f75006b3e645deb417299f75d916b78763bc37
strawberryjam <jamgithub@gmail.com>
2014-02-17 14:44:45 +0700

    Initial commit

---
+++ SaleManagement/WebFolder/supplier.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var btnSearch = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	btnSearch.click = function btnSearch_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("btnSearch", "click", btnSearch.click, "WAF");
+// @endregion
+};// @endlock
