3f825b014d263f61b77867bf78142fdf4af4bb47
minh <minhminh265@gmail.com>
2014-02-18 08:08:15 +0700

    minh add for new day

--- SaleManagement/WebFolder/Employee.waPage/scripts/index.js
+++ SaleManagement/WebFolder/Employee.waPage/scripts/index.js
@@ -2,10 +2,17 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var icoSClose = {};	// @icon
 	var btnAddNew = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	icoSClose.click = function icoSClose_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		$$('empcontainer').selectTab(1);
+	};// @lock
 
 	btnAddNew.click = function btnAddNew_click (event)// @startlock
 	{// @endlock
@@ -14,6 +21,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("icoSClose", "click", icoSClose.click, "WAF");
 	WAF.addListener("btnAddNew", "click", btnAddNew.click, "WAF");
 // @endregion
 };// @endlock
