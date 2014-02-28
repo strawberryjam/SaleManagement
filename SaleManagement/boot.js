9edf3979d578f371b5cc59f6bdb063eca452386b
minh <minhminh265@gmail.com>
2014-02-28 11:26:16 +0700

    minh update 2 file js boot va handler

---
+++ SaleManagement/boot.js
@@ -0,0 +1,4 @@
+﻿addHttpRequestHandler('/sendMail','handler.js','sendMail');
+    // add a request handler which
+    // calls the sendMail function in the handler.js file
+    // when the /sendMail request is received
